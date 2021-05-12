753a3ed00b560949f19c406a083abba9f6d13d37
onixtsm <alexander@sautkin.com>
2021-05-12 23:49:33 +0300

    ajax

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,30 +1,29 @@
+# frozen_string_literal: true
+# Blog class
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[ show edit update destroy ]
-  before_action :set_flairs
+  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_tags
 
-  def set_flairs
-    @flairs = ['Update', 'Opinion', nil]
+  def set_tags
+    @tags = ['Update', 'Opinion', nil]
   end
 
   # GET /blogs or /blogs.json
   def index
+    @blogs = Blog.order('created_at desc')
 
-    @blogs = Blog.order("created_at desc")
+    return params[:tag].blank?
+    @blogs = @blogs.where(tag: params[:tag])
 
-    if !(params[:flair].blank?)
-      @blogs = @blogs.where(:flair => params[:flair])
-
-      respond_to do |format|
-        format.html
-        format.js 
-        format.json 
-      end
+    respond_to do |format|
+      format.html
+      format.js
+      format.json
     end
   end
 
   # GET /blogs/1 or /blogs/1.json
-  def show
-  end
+  def show; end
 
   # GET /blogs/new
   def new
@@ -32,19 +31,15 @@ class BlogsController < ApplicationController
   end
 
   # GET /blogs/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /blogs or /blogs.json
   def create
-
-
     @blog = Blog.new(blog_params)
-
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: "Blog was successfully created." }
+        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
         format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -57,7 +52,7 @@ class BlogsController < ApplicationController
   def update
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: "Blog was successfully updated." }
+        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
         format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -70,19 +65,20 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: "Blog was successfully destroyed." }
+      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def blog_params
-      params.require(:blog).permit(:name, :title, :content, :flair)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def blog_params
+    params.require(:blog).permit(:name, :title, :content, :tag)
+  end
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,4 @@
 class Blog < ApplicationRecord
   validates :name, presence: true
   validates :title, presence: true
-  validates :flair, presence: true
 end
