56fa1910e1437d48988d0b5814a30abf25620682
onixtsm <alexander@sautkin.com>
2021-06-21 12:15:46 +0300

    added admin role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_155444) do
+ActiveRecord::Schema.define(version: 2021_06_21_090725) do
 
   create_table "add_name_to_users", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_06_20_155444) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
