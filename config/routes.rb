b60be96f83328a0b9b9d6284a9bf3a2e22a610c0
onixtsm <alexander@sautkin.com>
2021-08-21 01:07:50 +0300

    Added signing in option to nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: :registrations, controllers: { sessions: 'users/sessions' }
+  devise_for :users#, skip: :registrations, controllers: { sessions: 'users/sessions' }
   root 'home#index'
   get 'home/about'
 
