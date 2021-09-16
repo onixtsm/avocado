a4c9f3615ff52e9101c8900f5cd87104fc1f44b3
onixtsm <alexander@sautkin.com>
2021-09-17 01:24:13 +0300

    made home page more appealing

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -2,6 +2,6 @@ const { webpackConfig } = require('@rails/webpacker')
 // const { merge } = require('webpack-merge')
 const fs = require('fs')
 
-fs.writeFile('webpack.js.json', JSON.stringify(webpackConfig, undefined, 2), (err) => {console.log(err)})
+fs.writeFile('webpack.js.json', JSON.stringify(webpackConfig, undefined, 2), (err) => {if (err) {console.log(err)}})
 
 module.exports = webpackConfig
